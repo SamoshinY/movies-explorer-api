0c75b1772ca48d5959d946840a8b142d9a62e448
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-31 02:52:42 +0700

    выделить отдельный роут для удаления токена

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const signinRouter = require('./signin');
+const signoutRouter = require('./signout');
 const signupRouter = require('./signup');
 const PageNotFoundRouter = require('./non-existent-paths');
 const auth = require('../middlewares/auth');
@@ -9,6 +10,7 @@ const auth = require('../middlewares/auth');
 router
   .use('/signup', signupRouter)
   .use('/signin', signinRouter)
+  .use('/sigout', signoutRouter)
   .use('/users', auth, userRouter)
   .use('/movies', auth, movieRouter)
   .use('*', PageNotFoundRouter);
--- routes/signin.js
+++ routes/signin.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { signin } = require('../middlewares/request-validation');
-const { login, deleteJwt } = require('../controllers/users');
+const { login } = require('../controllers/users');
 
-router.post('/', signin, login).delete('/', deleteJwt);
+router.post('/', signin, login);
 
 module.exports = router;
