9f95225e29b0feda82aaa39ecc75b3061ed0b413
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-31 02:34:39 +0700

    устранить мелкие неточности

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,6 @@ const { bodyUser } = require('../middlewares/request-validation');
 
 const { getCurrentUser, updateProfile } = require('../controllers/users');
 
-userRouter
-  .get('/me', bodyUser, getCurrentUser)
-  .patch('/me', bodyUser, updateProfile);
+userRouter.get('/me', getCurrentUser).patch('/me', bodyUser, updateProfile);
 
 module.exports = userRouter;
