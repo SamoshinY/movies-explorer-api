948d33f334f513f13b6bcf8ad2dd752f2bd92e9c
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2024-04-03 19:09:49 +0700

    изменить параметры cors

--- utils/allowedCors.js
+++ utils/allowedCors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
   'http://movies.samoshin.pro',
-  'https://movies.samoshin.pro/',
+  'https://movies.samoshin.pro',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://192.168.1.230:3001',
