de7990a943bad105595beaed462deb179c8afd54
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2024-04-03 18:14:25 +0700

    изменить параметры cors

--- utils/allowedCors.js
+++ utils/allowedCors.js
@@ -1,8 +1,6 @@
 const allowedCors = [
-  // 'http://samoshin-diplom-front.nomoredomains.monster',
-  // 'https://samoshin-diplom-front.nomoredomains.monster',
   'http://movies.samoshin.pro',
-  'https://movies.samoshin.pro',
+  'https://movies.samoshin.pro/',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://192.168.1.230:3001',
